1fc14ac832f7fe8fad91025291a8bde0ba310a66
Meet15-tech <mt15062005@gmail.com>
2025-12-04 20:59:36 +0530

    Delete models directory

--- models/Customer.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const customerSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    phone: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Customer', customerSchema);
